b1f8ee4f3ae580037c6d2ce3ccec2e0294097686
bashy007 <bashiraremu474@gmail.com>
2024-09-17 15:18:17 +0100

    added-project-portfolio

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -5,11 +5,41 @@ import Image4 from "../../assets/Image4.png";
 import Image5 from "../../assets/Image5.png";
 import Image6 from "../../assets/Image6.png";
 import Image7 from "../../assets/Image7.png";
+import Image0 from "../../assets/image1_1.png"
+import Image001 from "../../assets/image1_2.png"
+import Image002 from "../../assets/fistula.png"
 // import Image8 from '../../assets/Image8.png'
 
 const data = [
   {
     id: 1,
+    category: "fullstack",
+    image: Image0,
+    title: "(FullStack)Ecommerce Fragnances",
+    desc: "React Tailwindcss React-Query",
+    demo: "https://mhkasa-front.vercel.app/",
+    github: "https://github.com/bashytech007/mhkasa-front",
+  },
+  {
+    id: 2,
+    category: "frontend",
+    image: Image001,
+    title: "Landing Page Built With React",
+    desc: "A landing page for a legal company coped during an hackhathon",
+    demo: "https://toyin.vercel.app/",
+    github: "https://github.com/bashytech007/Toyin",
+  },
+  {
+    id: 3,
+    category: "frontend",
+    image: Image002,
+    title: "An Ngo Site For VVF Cases built with  typescript and React",
+    desc: "A volunteer site about VVF CASES in Nigeria i built almost all the pages here(collaboration)",
+    demo: "https://fistulacareinitiative.org/",
+    github: "https://github.com/Olatade/fistulacareinitiative.org",
+  },
+  {
+    id: 4,
     category: "fullstack",
     image: Image1,
     title: "(FullStack)Ecommerce Boutique",
@@ -17,8 +47,9 @@ const data = [
     demo: "https://bash-clothing-j7kl1zsx3-bashytech007.vercel.app/",
     github: "https://github.com/bashytech007/bash-clothing",
   },
+  
   {
-    id: 2,
+    id: 5,
     category: "frontend",
     image: Image2,
     title: "ManageLandingPage(Frontend)",
@@ -27,7 +58,7 @@ const data = [
     github: "https://github.com/bashytech007/manageLandingPage",
   },
   {
-    id: 3,
+    id: 6,
     category: "frontend",
     image: Image3,
     title: "Keeper app(Frontend)",
@@ -36,7 +67,7 @@ const data = [
     github: "https://github.com/bashytech007/keeperApp",
   },
   {
-    id: 4,
+    id: 7,
     category: "frontend",
     image: Image4,
     title: "omniFood(Frontend)",
@@ -45,7 +76,7 @@ const data = [
     github: "https://github.com/bashytech007/omnifood",
   },
   {
-    id: 5,
+    id: 8,
     category: "frontend",
     image: Image5,
     title: "Recipeapp(Frontend)",
@@ -54,7 +85,7 @@ const data = [
     github: "https://github.com/bashytech007/Recipeapp",
   },
   {
-    id: 6,
+    id: 9,
     category: "frontend",
     image: Image6,
     title: "Bashsocials",
@@ -64,7 +95,7 @@ const data = [
   },
 
   {
-    id: 7,
+    id: 10,
     category: "frontend",
     image: Image7,
     title: "BATFARM(FRONTEND)",
@@ -72,15 +103,15 @@ const data = [
     demo: "https://thebatfarm.onrender.com/",
     github: "https://github.com/bashytech007/batfarm",
   },
-  //   {
-  //     id: 8,
-  //     category: "backend",
-  //     // image: Image8,
-  //     title: "FriendsApp(Backend)",
-  //     desc: "An api that can be used for friends",
-  //     demo: "image8",
-  //     github: "https://github.com/bashytech007/backendclass",
-  //   },
+    // {
+    //   id: 11,
+    //   category: "backend",
+    //   // image: Image8,
+    //   title: "FriendsApp(Backend)",
+    //   desc: "An api that can be used for friends",
+    //   demo: "image8",
+    //   github: "https://github.com/bashytech007/backendclass",
+    // },
 ];
 
 export default data;
