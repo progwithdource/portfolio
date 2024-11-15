c08b28c6ffdb8e06aa53f7550339258d5240d09b
progwithdource <idowuadeoluwa99@gmail.com>
2024-11-15 07:25:23 +0100

    newchanges

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-import './Card.css'
-
-function Card({children,className,onClick}) {
-  return (
-    <article className={`card ${className}`}onClick={onClick}>{children}</article>
-  )
-}
-
-export default Card
--- src/sections/faqs/Faqs.jsx
+++
@@ -1,9 +0,0 @@
-import './faqs.css'
-
-function Faqs() {
-  return (
-    <section id="faqs"><h2>Faqs</h2></section>
-  )
-}
-
-export default Faqs
